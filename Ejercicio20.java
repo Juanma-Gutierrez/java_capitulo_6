aeda4ca8e5cb8aad297fa2529295efbbef60eaa1
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-28 20:07:58 +0200

    feat: Ejercicio20

---
+++ Ejercicio20.java
@@ -0,0 +1,42 @@
+/**
+ * Exercise: 20
+ *
+ * @version: 28/10/2022 19:48
+ * @author: Juan Manuel Gutiérrez
+ *          https://github.com/Juanma-Gutierrez
+ */
+
+/*
+ * Realiza un programa que pinte por pantalla una cuba con cierta cantidad de
+ * agua. La capacidad será indicada por el usuario. La cuba se llenará con una
+ * cantidad aleatoria de agua que puede ir entre 0 y la capacidad máxima que
+ * pueda admitir. El ancho de la cuba no varía.
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio20 {
+    public static void main(String[] args) {
+        // Var declarations
+        int cap;
+        int water;
+
+        // Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        // Input data
+        System.out.print("Por favor, indique la capacidad de la cuba en litros: ");
+        cap = sc.nextInt();
+        water = (int) (Math.random() * (cap));
+
+        // Process data
+        for (int i = cap; i > water; i--)
+            System.out.println("|          |");
+        for (int i = water; i > 0; i--)
+            System.out.println("|~~~~~~~~~~|");
+        System.out.println("└──────────┘");
+        System.out.print("La cuba tiene una capacidad de " + cap + " litros y contiene " + water + " litros de agua.");
+
+        sc.close();
+    }
+}
--- README.md
+++ README.md
@@ -348,14 +348,27 @@ Media de todos los números: 53,34
 
 Realiza un programa que pinte por pantalla una cuba con cierta cantidad de agua. La capacidad será indicada por el usuario. La cuba se llenará con una cantidad aleatoria de agua que puede ir entre 0 y la capacidad máxima que pueda admitir. El ancho de la cuba no varía.
 
-### Ejemplo:
-
+```
+Ejemplos:
 Por favor, indique la capacidad de la cuba en litros: 3
-X    X
-X====X
-X====X
-XXXXXX
-La cuba tiene una capacidad de 3 litros y contiene 2 litros de agua.
+|          |
+|          |
+|          |
+|~~~~~~~~~~|
+|~~~~~~~~~~|
+|~~~~~~~~~~|
+|~~~~~~~~~~|
+|~~~~~~~~~~|
+└──────────┘
+La cuba tiene una capacidad de 8 litros y contiene 5 litros de agua.
+
+|          |
+|~~~~~~~~~~|
+|~~~~~~~~~~|
+|~~~~~~~~~~|
+└──────────┘
+La cuba tiene una capacidad de 4 litros y contiene 3 litros de agua.
+```
 
 ### Ejercicio 21
 
